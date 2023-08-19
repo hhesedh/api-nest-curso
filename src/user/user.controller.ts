d329bfed4cb910c4e7e6d7e0d42f72ad0bb47803
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-08-19 18:56:02 -0300

    adição de dto de validação para o patch e o put

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 
 @Controller('users')
 export class userController {
@@ -28,19 +30,29 @@ export class userController {
   }
 
   @Put(':id')
-  async update(@Body() body, @Param() params) {
+  async update(
+    @Body() { name, email, password }: UpdatePutUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'put',
-      body,
+      name,
+      email,
+      password,
       params,
     };
   }
 
   @Patch(':id')
-  async updatePartial(@Body() body, @Param() params) {
+  async updatePartial(
+    @Body() { name, email, password }: UpdatePatchUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'patch',
-      body,
+      name,
+      email,
+      password,
       params,
     };
   }
