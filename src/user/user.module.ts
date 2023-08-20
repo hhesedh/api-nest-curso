730da9f8f35ecb1078ec7d8c3be7f50574f7cd23
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-08-20 15:34:35 -0300

    criação e utilização do userService para criar usuário

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { userController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [userController],
-  providers: [],
+  providers: [UserService],
   exports: [],
 })
 export class UserModule {}
