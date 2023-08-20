c1e791c6f585faef072ff8bb8b444061a1219287
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-08-20 19:34:33 -0300

    criação de serviços PATCH e PUT para atualizar  o model usuário

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 
 @Injectable()
 export class UserService {
@@ -15,5 +17,27 @@ export class UserService {
 
   async show(id: number) {
     return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async update(id: number, { birthAt, ...restData }: UpdatePutUserDTO) {
+    const data = { ...restData, birthAt: birthAt ? new Date(birthAt) : null };
+    return this.prisma.user.update({
+      data,
+      where: { id },
+    });
+  }
+
+  async updatePartial(
+    id: number,
+    { birthAt, ...restData }: UpdatePatchUserDTO,
+  ) {
+    const data = {
+      ...restData,
+      ...(birthAt ? { birthAt: new Date(birthAt) } : {}),
+    };
+    return this.prisma.user.update({
+      data,
+      where: { id },
+    });
   }
 }
