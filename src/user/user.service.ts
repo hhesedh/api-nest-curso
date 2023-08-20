56fb54afaad77c298549e92244258b0b6430f717
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-08-20 16:08:17 -0300

    criação de get para usuários

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,4 +8,12 @@ export class UserService {
   async create(data: CreateUserDTO) {
     return this.prisma.user.create({ data });
   }
+
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async show(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
 }
