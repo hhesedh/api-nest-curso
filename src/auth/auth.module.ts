000558bc86c37db625c85398e6493cbe7268eeac
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-08-27 23:03:06 -0300

    criação do modulo do jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.APP_SECRET,
+    }),
+  ],
+})
+export class AuthModule {}
